26d8a7c47b39fa7328fb241037acb76ee7349fd9
Salmiak <alfred@salmiakmedia.se>
2017-09-21 21:46:27 +0200

    Mexico version
    Higher threshold to show WiFi, and also only WiFi with venue. Loading
    more WiFi on first request.

--- src/wtfwifi.js
+++ src/wtfwifi.js
@@ -2,7 +2,8 @@ var APIbase = 'https://api.instabridge.com/lite/';
 var APItoken = '&token=0a00333dfb10f99badc647af68586a17c9dcf14242970d81bc96b1af5e6b23bf81aec823ad27f153';
 
 var offset = 0;
-var radius = 100;
+var perRequest = 100;
+var radius = 500;
 var userPos = false;
 var blackList = []
 
@@ -10,12 +11,27 @@ var app = new Vue({
   el: '#content',
   data: {
     message: '<br/>Loading...',
-    result: false
+    result: false,
+    results: [],
+    listOffset: -1
   },
   methods: {
+    setWiFi: function(wifi) {
+      this.result = wifi
+      this.message = false;
+    },
     getNextWiFi: function () {
       ga('send', 'event', 'Button', 'Other WiFi');
-      getHotspotsAtLocation(userPos, offset);
+
+      this.listOffset++;
+      blackList.push(this.result.id);
+
+      if (this.results.length > this.listOffset)
+        this.setWiFi(this.results[this.listOffset]);
+      else {
+        this.listOffset = 0;
+        this.setWiFi(this.results[this.listOffset]);
+      }
     },
     gaDirections: function() {
       ga('send', 'event', {
@@ -36,50 +52,59 @@ var app = new Vue({
 
 function getHotspotsAtLocation(position, _offset) {
 
+  position = {
+    coords: {
+      latitude:19.4326,
+      longitude:-99.1332
+    }
+  };
   app.result = false;
-  app.message = "Finding the best fucking WiFi...";
+  app.results = [];
+  app.message = "Finding the best WiFi...";
+  addedWiFi = false;
 
-  var url = APIbase + 'hotspots?radius=' + radius + '&offset=' + _offset + '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&limit=1' + APItoken;
+  var url = APIbase + 'hotspots?radius=' + radius + '&offset=' + _offset + '&lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&limit=' + perRequest + APItoken;
 
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
+
       var responseObj = JSON.parse(this.responseText);
+
+      // No WiFi in results - redo search bigger area
       if(responseObj.hotspots.length == 0) {
-
-        radius += 100;
+        radius += 500;
         offset = 0;
-        getHotspotsAtLocation(position, offset)
-
-      } else if (blackList.indexOf(responseObj.hotspots[0].id) != -1 ) {
-
-        offset++;
-        getHotspotsAtLocation(position, offset)
-
-      } else if ( responseObj.hotspots[0].p_exists * responseObj.hotspots[0].p_internet < 0.75 ) {
-
-        blackList.push(responseObj.hotspots[0].id)
-        offset++;
-        getHotspotsAtLocation(position, offset)
-
-      } else {
-
-        app.result = responseObj.hotspots[0];
-        app.result.directionsUrl = 'https://www.google.com/maps/dir/Current+location/'+app.result.lat+','+app.result.lon;
-        app.message = false;
-        blackList.push(app.result.id);
-        offset++;
-
-        ga('send', 'event', 'WiFi', 'Got wifi', {
-          nonInteraction: true
-        });
-
+        return getHotspotsAtLocation(position, offset);
       }
+
+      ga('send', 'event', 'WiFi', 'Got wifi', {
+        nonInteraction: true
+      });
+
+      for (wifi of responseObj.hotspots) {
+        if( blackList.indexOf(wifi.id) == -1 ) {
+          blackList.push(wifi.id)
+          if ( wifi.p_exists * wifi.p_internet > 0.75 && wifi.venue_name ) {
+            wifi.directionsUrl = 'https://www.google.com/maps/dir/Current+location/'+wifi.lat+','+wifi.lon;
+            wifi.staticMapSrc = 'https://maps.googleapis.com/maps/api/staticmap?center='+wifi.lat+','+wifi.lon+'&zoom=15&size=480x320&scale=2&markers=color:0xFF66A4|'+wifi.lat+','+wifi.lon+'&key=AIzaSyDSBTRvomePfdfDVb14j7S5etk7B4cI7RA'
+            app.results.push(wifi);
+            addedWiFi = true;
+          }
+        }
+      }
+      if(addedWiFi) {
+        app.getNextWiFi();
+      } else {
+        offset += perRequest;
+        getHotspotsAtLocation(position, offset);
+      }
+
     } else if (this.readyState == 4 && this.status != 200) {
-      app.message = "Something fucked up - try again later :(";
+      app.message = "Something whent wrong - try again later :(";
     } else {
-      app.message = "Finding the best fucking WiFi...";
+      app.message = "Finding a working WiFi...";
     }
   }
 
@@ -90,7 +115,7 @@ function getHotspotsAtLocation(position, _offset) {
 function getWiFi(offset) {
   app.result = false;
   if (navigator.geolocation) {
-    app.message = "Fetching your fucking location..."
+    app.message = "Fetching your location..."
     navigator.geolocation.getCurrentPosition(function(position) {
       ga('send', 'event', 'Location', 'Got location', {
         nonInteraction: true
@@ -99,7 +124,7 @@ function getWiFi(offset) {
       getHotspotsAtLocation(userPos, offset);
     });
   } else {
-    app.message = "Geolocation is not supported by this fucking browser.";
+    app.message = "Geolocation is not supported by this browser.";
   }
 }
 
